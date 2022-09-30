f8f70895818adcdce3c502efef0f94003c4fac0f
sabrinagarciia <sabrina.garcia@mercadolibre.com>
2022-09-30 01:00:32 -0300

    primer ejercicio

--- internal/file/file.go
+++ internal/file/file.go
@@ -2,7 +2,9 @@ package file
 
 import (
 	"encoding/csv"
+	"io"
 	"os"
+	//"strings"
 
 	"github.com/sabrinagarciia/hackathon-go-bases-main/internal/service"
 )
@@ -14,25 +16,43 @@ type File struct {
 // apunto a la dirección de la estructura (struct) File
 // esto es para actualizar directamente la estructura y no una copia
 
-func (f *File) Read() ([]service.Ticket, error) {
+func (f *File) Read() (/*[]service.Ticket*/ []string, error) {
+	// abro el archivo desde su path
 	file, err := os.Open("../../tickets.csv")
 
 	if err != nil {
-		panic("path incorrecto")
+		return nil, err
 	}
 
+	// creo el lector del archivo
 	reader := csv.NewReader(file)
+
+	// leo linea por linea
 	for {
 		records, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		for _, record := range records {
-			ticket := service.Ticket {
-				Names: record[0],
+		if err != nil {
+			if err == io.EOF {
+				break
 			}
+			return nil, err
 		}
+
+		return records, nil
 	}
+
+	//info := strings.Split(string(file), "\n")
+
+	// for {
+	// 	records, err := reader.Read()
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// 	for _, record := range records {
+	// 		ticket := service.Ticket {
+	// 			Names: record[0],
+	// 		}
+	// 	}
+	// }
 	return nil, nil
 }
 
